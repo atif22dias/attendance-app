62774887ef46de03dabded0a56607bd645f7c0c3
zaiddias <mailzaid2000@gmail.com>
2026-05-13 12:59:07 +0530

    fix railway mysql env variables

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ import os
 import mysql.connector
 
 db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    port=os.getenv("DB_PORT", 3306)
+    host=os.getenv("MYSQLHOST"),
+    user=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    database=os.getenv("MYSQLDATABASE"),
+    port=int(os.getenv("MYSQLPORT", 3306))
 )
 
 cursor = db.cursor()
